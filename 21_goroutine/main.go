5c5354add136f9cef05b8916dbb915fb1530a39c
kemuxiaozi000 <skcherryblossom@163.com>
2020-05-31 23:46:24 +0800

    channel

--- 21_goroutine/main.go
+++ 21_goroutine/main.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 	"time"
 )
 
 func main() {
-	var a [10]int
-	for i := 0; i < 10; i++ {
+	// var a [10]int
+	for i := 0; i < 1000; i++ {
 		func(i int) {
 			for {
-				a[i]++
-				runtime.Gosched()
+				// a[i]++
+				// runtime.Gosched()
+				fmt.Printf("Hello from "+"goroutine %d\n", i)
 			}
 		}(i)
 	}
 	time.Sleep(time.Millisecond)
-	fmt.Println(a)
+	// fmt.Println(a)
 }
---
+++ 22_channel/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func worker(id int, c chan int) {
+	for {
+		fmt.Printf("worker %d received %c\n", id, <-c)
+	}
+}
+
+func channelDemo() {
+	var channels [10]chan int
+	for i := 0; i < 10; i++ {
+		channels[i] = make(chan int)
+		go worker(i, channels[i])
+	}
+
+	for i := 0; i < 10; i++ {
+		channels[i] <- 'a' + i
+
+	}
+	for i := 0; i < 10; i++ {
+		channels[i] <- 'A' + i
+
+	}
+	time.Sleep(time.Millisecond)
+}
+
+func main() {
+	channelDemo()
+}
