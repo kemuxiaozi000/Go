d9bbabd1ff7166ed1929ddec5f91591672812b1d
sunke <skcherryblossom@163.com>
2020-06-08 09:22:48 +0800

    http

--- 27_http/main.go
+++ 27_http/main.go
@@ -10,9 +10,15 @@ import (
 
 func main() {
 	request, err := http.NewRequest(http.MethodGet, "http://www.imooc.com", nil)
-	request = request.Header.Add()
-	resp, err := http.DefaultClient.Do(request)
-	resp, err := http.Get("http://www.baidu.com")
+	request.Header.Add("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1")
+	// resp, err := http.DefaultClient.Do(request)
+	client := http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			fmt.Println("Redirect:", req)
+			return nil
+		},
+	}
+	resp, err := client.Do(request)
 	if err != nil {
 		panic(err)
 	}
---
+++ 28_BFS/main.go
@@ -0,0 +1,7 @@
+package main
+
+// 广度优先算法
+
+func main() {
+
+}
